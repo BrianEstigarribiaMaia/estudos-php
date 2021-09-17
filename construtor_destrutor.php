121dc5e8b6c9bd141405537374dafc1139c8347f
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-09-17 19:26:27 -0300

    Add files via upload

---
+++ construtor_destrutor.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+class Pessoa{
+	
+	public $Codigo;
+	public $Nome;
+	
+	//método construtor
+	function __construct($codigo, $nome){
+		$this->Codigo = $codigo;
+		$this->Nome   = $nome;
+	}
+	
+	//método destrutor
+	function __destruct(){
+		echo "deslocando {$this->Nome}\n";
+	}
+	
+}
+
+$maria = new Pessoa(27, 'Maria da Silva');
+$joana = new Pessoa(28, 'Joana Maranhão');
+
+var_dump($maria, $joana);
+
+unset($maria);
+unset($joana);
+?>
